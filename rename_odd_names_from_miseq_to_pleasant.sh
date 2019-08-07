b404f68a9be731ce3142b0ba75da5d3ad8d70ff7
SillySabertooth <danatyermakovich@gmail.com>
2019-08-07 15:29:11 +0300

    working_for_letters_too

--- rename_odd_names_from_miseq_to_pleasant.sh
+++ rename_odd_names_from_miseq_to_pleasant.sh
@@ -2,14 +2,24 @@
 
 #очень древнняя штука; не помню, почему не sed
 #странно раотает; но работает ведь!
-
-read Numb
-for i in $(seq 1 $Numb)
+#
+#read Numb
+#for i in $(seq 1 $Numb)
+#do
+#for R in 1 2
+#do
+#rename "s/${i}_S[0-9]*_L001_R${R}_001.fastq.gz/${i}.R${R}.fastq.gz/" *
+#done
+#done
+#
+###теперь работает не только для цифр
+for n in $(ls *.fastq.gz)
 do
+i="${n%_*_*_*_*}"
+echo ${i}
 for R in 1 2
 do
 rename "s/${i}_S[0-9]*_L001_R${R}_001.fastq.gz/${i}.R${R}.fastq.gz/" *
 done
 done
-
 
